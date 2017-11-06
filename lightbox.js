e7b9fbd5b4dc6faf2613091bb8aface62f84e977
Kristen Schoeck <kristen.schoeck@gmail.com>
2017-11-05 21:46:04 -0800

    Add files via upload
    Hi William -
    Attached are my files for the JQuery lightbox code challenge. Thanks,
    Kristen Schoeck

--- lightbox.js
+++ lightbox.js
@@ -1,14 +1,26 @@
-function Lightbox( options )
-{
-  // your code here
-}
 
-Lightbox.prototype.open = function()
-{
-  // your code here
-};
-
-Lightbox.prototype.close = function()
-{
-  // your code here
-};
+			$(document).ready(function(){
+ 
+				$('.lightbox').click(function(){
+					$('.backdrop, .box').animate({'opacity':'.50'}, 300, 'linear');
+					$('.box').animate({'opacity':'1.00'}, 300, 'linear');
+					$('.backdrop, .box').css('display', 'block');
+				});
+ 
+				$('.close').click(function(){
+					close_box();
+				});
+ 
+				$('.backdrop').click(function(){
+					close_box();
+				});
+ 
+			});
+ 
+			function close_box()
+			{
+				$('.backdrop, .box').animate({'opacity':'0'}, 300, 'linear', function(){
+					$('.backdrop, .box').css('display', 'none');
+				});
+			}
+ 
